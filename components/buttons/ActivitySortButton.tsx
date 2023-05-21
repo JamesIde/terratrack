8deea86d6c5f9ed66e06fc03687d8d054f17c9e1
James <james.ide775@gmail.com>
2023-05-21 11:04:29 +0930

    This should be in buttons

--- components/buttons/ActivitySortButton.tsx
+++ components/buttons/ActivitySortButton.tsx
@@ -1,6 +1,6 @@
 import { View, Text, StyleSheet } from "react-native";
 import { FontAwesome } from "@expo/vector-icons";
-import { globalColors } from "../../../global/styles/globalColors";
+import { globalColors } from "../../global/styles/globalColors";
 export default function ActivitySortButton() {
   return (
     <View style={{ alignItems: "flex-end", paddingRight: 15, marginTop: 20 }}>
